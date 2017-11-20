1ad78683b8ca4ca50d90ca058dca5b519f917e57
Presiden <hanirizo@gmail.com>
2017-11-20 21:13:45 +0700

    init

---
+++ app.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START use_task_and_queue]
+// [START use_task]
+use google\appengine\api\taskqueue\PushTask;
+// [END use_task]
+use google\appengine\api\taskqueue\PushQueue;
+// [END use_task_and_queue]
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+// create the Silex application
+$app = new Application();
+
+$app->get('/', function () use ($app) {
+    // [START add_task]
+    
+    $task = new PushTask(
+       '/worker',
+        ['name' => 'john doe', 'action' => 'send_reminder']
+    );
+    $task_name = $task->add();
+    
+    //$task1 = new PushTask('/worker');
+    //$queue = new PushQueue();
+    //$queue->addTasks([$task1]);
+    
+    
+    // [END add_task]
+    // [START add_tasks]
+    //$task1 = new PushTask('/someUrl');
+    //$task2 = new PushTask('/someOtherUrl');
+    //$queue = new PushQueue();
+    //$queue->addTasks([$task1, $task2]);
+    // [END add_tasks]
+    // [START url_endpoints]
+    //(new PushTask('/worker', ['data_for_task' => 1234]))->add();
+    // [END url_endpoints]
+    return 'A task ' . $task_name . ' added.';
+});
+
+$app->post('/worker', function (Request $req) use ($app) {
+
+	$telegramapi="334808005:AAFG5Bw4swD1ZPTPkQ80TRGjkFDiXwVoGvk";
+	$telegramchat="124071029";
+	//file_get_contents("https://api.telegram.org/bot$telegramapi/sendMessage?text=test&chat_id=$telegramchat");
+	//echo 'kopet';
+    //return 'name: ' . $req->get('name') . "\n"
+     ///   . 'action: ' . $req->get('action');
+     shell_exec('mkdir kopet');
+     unlink('kopet');
+});
+
+$app->get('/kopet', function (Request $req) use ($app) {
+
+	$memcache = new Memcache;
+	$memcache->set('kopet','yes');
+	$memcache->set('sekolah','wes');
+	
+    return 'Ok';
+});
+
+$app->post('/someOtherUrl', function (Request $req) use ($app) {
+    return 'Ok';
+});
+
+$app->post('/path/to/my/worker', function (Request $req) use ($app) {
+    return 'Ok';
+});
+
+return $app;
